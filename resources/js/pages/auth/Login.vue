0b37e5c848c4b5cbc6821724d677b6e203340143
Pavel Borunov <info@delho.ee>
2025-10-02 16:04:16 +0300

    Disable registration

--- resources/js/pages/auth/Login.vue
+++ resources/js/pages/auth/Login.vue
@@ -7,7 +7,7 @@ import { Checkbox } from '@/components/ui/checkbox';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import AuthBase from '@/layouts/AuthLayout.vue';
-import { register } from '@/routes';
+// import { register } from '@/routes';
 import { request } from '@/routes/password';
 import { Form, Head } from '@inertiajs/vue3';
 import { LoaderCircle } from 'lucide-vue-next';
@@ -100,10 +100,10 @@ defineProps<{
                 </Button>
             </div>
 
-            <div class="text-center text-sm text-muted-foreground">
-                Don't have an account?
-                <TextLink :href="register()" :tabindex="5">Sign up</TextLink>
-            </div>
+<!--            <div class="text-center text-sm text-muted-foreground">-->
+<!--                Don't have an account?-->
+<!--                <TextLink :href="register()" :tabindex="5">Sign up</TextLink>-->
+<!--            </div>-->
         </Form>
     </AuthBase>
 </template>
